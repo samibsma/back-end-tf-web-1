232d98560997e853b4330626a9c58e323568a4a8
Sâmi <132698237+samibsma@users.noreply.github.com>
2023-12-12 01:16:18 +0000

    modelo conceitual

--- visitante.js
+++ visitante.js
@@ -1,8 +1,5 @@
-javascript
+
 app.get('/api/visitante/cardapio', (req, res) => {
-  // Lógica para buscar e retornar o cardápio
-});
 
 app.post('/api/visitante/feedback', (req, res) => {
-  // Lógica para adicionar o feedback ao banco de dados
 });
